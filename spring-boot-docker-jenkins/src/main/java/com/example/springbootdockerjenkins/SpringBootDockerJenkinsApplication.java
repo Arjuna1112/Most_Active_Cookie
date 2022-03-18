ff485ce3d9565bdf0cf5f9ec3c2577285567dd76
Arjuna Balaji <arjuna.b45@gmail.com>
2022-03-18 20:22:56 +0530

    Adding new spring boot project to test docker image creation and push to hub using jenkins
    A

---
+++ spring-boot-docker-jenkins/src/main/java/com/example/springbootdockerjenkins/SpringBootDockerJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinsApplication.class, args);
+	}
+
+}
